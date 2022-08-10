ba851e3335dc8bc2bac08dd89864c89be63378d1
Zade Viggers <zade.viggers@gmail.com>
2022-08-11 10:08:41 +1200

    Basic player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,11 +10,13 @@ public class PlayerController : MonoBehaviour
     public float jumpSpeed;
 
     private Rigidbody2D rb;
+    private new Collider2D collider;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -25,11 +28,26 @@ public class PlayerController : MonoBehaviour
         float horizontalMovement = 0;
         if (horizontalInput != 0) horizontalMovement = Time.deltaTime * walkSpeed;
         if (horizontalInput < 0) horizontalMovement = horizontalMovement * -1;
+
         float verticalMovement = 0;
-        if (verticalInput > 0) verticalMovement = jumpSpeed;
+        Debug.Log(IsGrounded());
+        if (verticalInput > 0 && IsGrounded()) verticalMovement = jumpSpeed;
 
         Vector2 movement = new Vector2(horizontalMovement, verticalMovement);
 
+        rb.AddForce(movement);
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Deadly")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        float distToGround = collider.bounds.extents.y;
+        return Physics2D.Raycast(transform.position, -Vector3.up, (float)(distToGround + 0.1));
     }
 }
