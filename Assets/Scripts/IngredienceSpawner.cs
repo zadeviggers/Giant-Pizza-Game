79908710ce1f222393f6bbac9e825da9cec6550b
Zade Viggers <zade@viggers.net>
2022-08-14 18:46:20 +1200

    Tidy some stuff up

--- Assets/Scripts/IngredienceSpawner.cs
+++ Assets/Scripts/IngredienceSpawner.cs
@@ -7,36 +7,45 @@ public class IngredienceSpawner : MonoBehaviour
     public List<GameObject> thingsToSpawn;
     public float spawnInterval;
     public float spawnWidth;
+    private float spawnOffsetDistance;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnObstaclesCorutine());
+        spawnOffsetDistance = spawnWidth / 2;
+        StartCoroutine(SpawnObstaclesCoroutine());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        float psawnOffsetDistance = spawnWidth / 2;
-        Vector2 leftmostPoint = new Vector2(transform.position.x - psawnOffsetDistance, transform.position.y);
-        Vector2 rightmostPoint = new Vector2(transform.position.x + psawnOffsetDistance, transform.position.y);
-        Debug.DrawLine(leftmostPoint, rightmostPoint, Color.green, 1);
-    }
 
-    IEnumerator SpawnObstaclesCorutine()
+    IEnumerator SpawnObstaclesCoroutine()
     {
         int indexToSpawn = Random.Range(0, thingsToSpawn.Count);
 
         GameObject thingToSpawn = thingsToSpawn[indexToSpawn];
 
-        float psawnOffsetDistance = spawnWidth / 2;
-        float spawnX = Random.Range(transform.position.x - psawnOffsetDistance, transform.position.x + psawnOffsetDistance);
+        float spawnX = Random.Range(transform.position.x - spawnOffsetDistance, transform.position.x + spawnOffsetDistance);
         Vector2 spawnPosition = new Vector2(spawnX, transform.position.y);
 
         Instantiate(thingToSpawn, spawnPosition, Quaternion.identity);
 
         yield return new WaitForSeconds(spawnInterval);
 
-        StartCoroutine(SpawnObstaclesCorutine());
+        StartCoroutine(SpawnObstaclesCoroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawSpawnDebugLine();
+    }
+
+
+    void DrawSpawnDebugLine()
+    {
+        Vector2 leftmostPoint = new Vector2(transform.position.x - spawnOffsetDistance, transform.position.y);
+        Vector2 rightmostPoint = new Vector2(transform.position.x + spawnOffsetDistance, transform.position.y);
+        Debug.DrawLine(leftmostPoint, rightmostPoint, Color.green, 1);
+
     }
 }
