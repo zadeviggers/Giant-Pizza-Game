3856109186d4a669679fb0b63aecbe976df74843
Zade Viggers <zade@viggers.net>
2022-08-14 19:25:18 +1200

    Health system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,10 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
 
+    // Player health points. Each point is "half a heart".
+    public int health;
+
+    // Set in editor
     public float walkSpeed;
     public float jumpSpeed;
 
@@ -30,24 +34,62 @@ public class PlayerController : MonoBehaviour
         if (horizontalInput < 0) horizontalMovement = horizontalMovement * -1;
 
         float verticalMovement = 0;
-        Debug.Log(IsGrounded());
-        if (verticalInput > 0 && IsGrounded()) verticalMovement = jumpSpeed;
+        // Debug.Log(IsGrounded());
+        // if (verticalInput > 0 && IsGrounded()) verticalMovement = jumpSpeed;
 
         Vector2 movement = new Vector2(horizontalMovement, verticalMovement);
 
         rb.AddForce(movement);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    // bool IsGrounded()
+    // {
+    //     float distToGround = collider.bounds.extents.y;
+    //     return Physics2D.Raycast(transform.position, -Vector3.up, (float)(distToGround + 0.1));
+    // }
+
+    void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.CompareTag("Deadly")) {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (other.CompareTag("Painful"))
+        {
+            Destroy(other.gameObject);
+            TakeDamage();
+            return;
+        }
+
+        if (other.CompareTag("Healing"))
+        {
+            Destroy(other.gameObject);
+            HealDamage();
+            return;
         }
     }
 
-    bool IsGrounded()
+    void OnCollisionEnter2D(Collision2D other)
     {
-        float distToGround = collider.bounds.extents.y;
-        return Physics2D.Raycast(transform.position, -Vector3.up, (float)(distToGround + 0.1));
+        if (other.collider.CompareTag("VeryPainful"))
+        {
+            TakeDamage(2);
+            return;
+        }
+    }
+
+    void TakeDamage(int amount = 1)
+    {
+        health -= amount;
+        if (health < 1)
+            Die();
+    }
+
+    void HealDamage(int amount = 1)
+    {
+        health += amount;
+    }
+
+    void Die()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
     }
 }
