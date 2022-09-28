0cdce9bea935ccbb1ac63e669ce567cbe86c6b4b
Zade Viggers <zade.viggers@gmail.com>
2022-09-29 09:39:32 +1300

    Menus

--- Assets/Scripts/GameManagerProxy.cs
+++ Assets/Scripts/GameManagerProxy.cs
@@ -1,25 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class MenuButton : MonoBehaviour
+public class GameManagerProxy : MonoBehaviour
 {
-    // Game manager instance
     GameManager gameManager;
-
     // Start is called before the first frame update
     void Start()
     {
-        // Get the game manager instance
         gameManager = GameManager.GetGameManager();
     }
 
-    // Called when the button is pressed
-    private void OnMouseUpAsButton()
+    public void GoToMenu()
     {
-        Debug.Log("Clicked!");
-        // Go to the menu on click
         gameManager.GoToMenu();
+    }
+
+    public void StartGame()
+    {
+        gameManager.StartGame();
     }
 }
