1c670a4cf45b09cb0880ff48773c8a1fbece84db
Zade Viggers <zade.viggers@gmail.com>
2022-10-20 09:22:08 +1300

    Make ground move via a super scuffed method

--- Assets/Scripts/GroundScroller.cs
+++ Assets/Scripts/GroundScroller.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveWithLevel : MonoBehaviour
+public class GroundScroller : MonoBehaviour
 {
-    public static Vector3 MoveAmount = new Vector3(0.1f, 0);
     GameManager gameManager;
+
+    SpriteRenderer sprite;
 
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameManager.GetGameManager();
+        sprite = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
@@ -19,7 +22,8 @@ public class MoveWithLevel : MonoBehaviour
         // Only move while the game is being played
         if (gameManager.currentState != GameState.Playing) return;
 
-        // Use Vector3 instead of Vector2 to account for layering
-        transform.position = new Vector3(transform.position.x + MoveAmount.x,  transform.position.y + MoveAmount.y, transform.position.z);
+        // Super scuffed way of making the ground look like it's moving
+        // This will probably eventually cause a crash because the sprite gets too big
+        sprite.size = new Vector2(sprite.size.x + MoveWithLevel.MoveAmount.x, sprite.size.y);
     }
 }
