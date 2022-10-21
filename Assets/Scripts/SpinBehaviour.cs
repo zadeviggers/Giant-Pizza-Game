d612f0aadea9eadc1d83accea8a8f38202c33ff6
Zade Viggers <zade@viggers.net>
2022-10-21 15:14:10 +1300

    Add spinning to peperoni

--- Assets/Scripts/SpinBehaviour.cs
+++ Assets/Scripts/SpinBehaviour.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GiantBallBehaviour : MonoBehaviour
+public class SpinBehaviour : MonoBehaviour
 {
     public float rotationAmount;
     private Vector3 rotation;
